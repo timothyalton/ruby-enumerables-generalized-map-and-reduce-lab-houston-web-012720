7efb3a37ff1b2323fa44c50b1f5e8fb476ca8cfe
Mitchell T Alton <mitchell.alton@live.com>
2020-01-24 21:41:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,8 +16,7 @@ def reduce(array, starting_value=nil)
    total = array[i]
    i = 1 
  end 
- 
- while i < array.length do 
+  while i < array.length do 
    total = yield(total, array[i])
    i += 1 
  end 
